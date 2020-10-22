f082b4b0f0decba5dd5b039dc1d54349c0434799
Francisco-Rola <francisco.rola@tecnico.ulisboa.pt>
2020-10-22 09:22:09 +0100

    fix vertexphi

--- thesis/src/thesis/VertexPhi.java
+++ thesis/src/thesis/VertexPhi.java
@@ -7,11 +7,12 @@ import java.util.Map;
 import javafx.util.Pair;
 
 public class VertexPhi {
-	
+	// set of variables and its ranges
 	private HashMap<String, Pair<Integer, Integer>> variables = null;
-	
+	// scale factor for TPC -C
 	private static int w = 2;
 	
+	// constructor for deep copy purpose
 	public VertexPhi(VertexPhi vertexPhi) {
 		this.variables = new HashMap<>();
 		for (Map.Entry<String, Pair<Integer, Integer>> entry : vertexPhi.getPhi().entrySet()) {
@@ -21,10 +22,11 @@ public class VertexPhi {
 		}
 	}
 	
+	// default constructor for vertex phi
 	public VertexPhi (HashSet<String> rhoVariables) {
-		
+		// initialize variables map
 		this.variables = new HashMap<>();
-		
+		// from all the variables in the corresponding rho, initialize its range
 		for (String variable : rhoVariables) {
 			if (variable.equals("districtid")) {
 				Pair<Integer, Integer> range = new Pair<Integer,Integer>(new Integer(1), new Integer(w*10));
@@ -51,15 +53,18 @@ public class VertexPhi {
 				this.variables.put(variable, range);
 			}
 			else {
+				// debug scenario, missing variable case
 				System.out.println("Missing case -> " + variable);
 			}
 		}
 	}
 	
+	// method using to extract set of variables in a phi and its ranges
 	public HashMap<String, Pair<Integer, Integer>> getPhi() {
 		return this.variables;
 	}
 	
+	// method that returns vertex phi as a set of conjunctions comprising all its variables and ranges
 	public String getPhiAsString() {
 		String phi = "";
 		for (Map.Entry<String, Pair<Integer,Integer>> entry: variables.entrySet()) {
@@ -69,6 +74,7 @@ public class VertexPhi {
 		return phi;
 	}
 	
+	// method that returns vertex phi as a sring that groups variables and its ranges in a string list
 	public String getPhiAsGroup() {
 		String phiQuery = "";
 		// find each phi associated wit the rho
@@ -79,6 +85,7 @@ public class VertexPhi {
 		return phiQuery;
 	}
 	
+	// schema helper, obtain table max range for vertex splitting purposes
 	public static int getTableRange(int tableNo) {
 		switch (tableNo) {
 		case 8:
@@ -86,17 +93,20 @@ public class VertexPhi {
 		case 9:
 			return w * w;
 		default:
+			// debug scenario, if needed add more tables
 			System.out.println("\n\nTable range unknown\n\n");
 			return -1;
 		}
 	}
 	
+	// debug method, prints phi, all its variables and ranges
 	public void printPhi() {
 		for (Map.Entry<String, Pair<Integer, Integer>> entry: this.getPhi().entrySet()) {
 			System.out.println("(" + entry.getKey() + ", " + entry.getValue().getKey() + ", " + entry.getValue().getValue() + ")");
 		}
 	}
 
+	// automatically generated hash code for vertex phi
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -105,6 +115,7 @@ public class VertexPhi {
 		return result;
 	}
 
+	// automatically generated equals for vertex phi
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
