9d8134725a361ca07dec293d003554815fd0893e
Francisco-Rola <francisco.rola@tecnico.ulisboa.pt>
2024-07-27 22:19:33 +0100

    rubis test

--- src/thesis/RubisWorkloadGenerator.java
+++ src/thesis/RubisWorkloadGenerator.java
@@ -388,7 +388,7 @@ public class RubisWorkloadGenerator {
 		// part the access belongs to
 		int part = -1;
 		
-		int randPart = ThreadLocalRandom.current().nextInt(0, 1);
+		int randPart = ThreadLocalRandom.current().nextInt(0, 2);
 
 		if (randPart == 1)
 			return false;
