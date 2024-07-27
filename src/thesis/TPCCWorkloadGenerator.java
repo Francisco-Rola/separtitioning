6bdc9508424cab3e21021095bc77627f508d24e1
Francisco-Rola <francisco.rola@tecnico.ulisboa.pt>
2024-07-27 19:21:02 +0100

    fix 1w5p

--- src/thesis/TPCCWorkloadGenerator.java
+++ src/thesis/TPCCWorkloadGenerator.java
@@ -1077,10 +1077,10 @@ public class TPCCWorkloadGenerator {
 		}
 		
 		// extra situation for 1w workloads, the warehouse item is always in part 0
-		if (table == 1 && VertexPhi.getScalingFactorW() == 1) {
+		if (table == 1 && noP == 2 && VertexPhi.getScalingFactorW() == 1) {
 			part = 0;
 		}
-		else if (table == 1 && VertexPhi.getScalingFactorW() == 1) {
+		else if (table == 1 && noP == 5 && VertexPhi.getScalingFactorW() == 1) {
 			part = 4;
 		}
 		// extra situation for 1w workloads, the table 9 is always mapped to partition 1
